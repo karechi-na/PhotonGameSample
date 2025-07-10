2508d0bfc4947abffd3d40e0326a681f6689ae17
Jun Shimura <junshimura@gmail.com>
2025-07-10 17:27:44 +0900

    Player Hasstate

--- Assets/PhotonGameSample/Prefabs/ItemCatcher.cs
+++ Assets/PhotonGameSample/Prefabs/ItemCatcher.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections;
 using UnityEngine;
+using Fusion;
 
 [RequireComponent(typeof(PlayerAvatar))]
 public class ItemCatcher : MonoBehaviour
 {
-    public event Action<Item,PlayerAvatar> OnItemCaught; // Event to notify when an item is caught
+    public event Action<Item, PlayerAvatar> OnItemCaught; // Event to notify when an item is caught
     private PlayerAvatar playerAvatar;
     private void Awake()
     {
@@ -13,9 +14,16 @@ public class ItemCatcher : MonoBehaviour
     }
     public void ItemCought(Item item)
     {
-        Debug.Log($"Item caught by {playerAvatar.NickName.Value}");
-        
-        // アイテムをキャッチしたときの処理
-        OnItemCaught?.Invoke(item, playerAvatar);
+        if (playerAvatar.Object.HasStateAuthority)
+        {
+            Debug.Log($"Item caught by {playerAvatar.NickName.Value}");
+
+            // アイテムをキャッチしたときの処理
+            OnItemCaught?.Invoke(item, playerAvatar);
+        }
+        else
+        {
+            Debug.LogWarning("ItemCatcher does not have state authority to catch items.");
+        }
     }
 }
